3665b6ed045eb73a39146df1316198533e35f9c6
DanielElisenberg <Daniel@Elisenberg.com>
2025-12-01 08:02:05 +0100

    2025: day 1 - refactor & cleanup

--- 2025/python/src/day1/day1.py
+++ 2025/python/src/day1/day1.py
@@ -1,59 +1,45 @@
-from typing import Literal
 from collections import deque
 
 
-dir = Literal["L"] | Literal["R"]
-
-
-def read_instructions() -> list[tuple[dir, int]]:
+def read_instructions() -> list[int]:
     with open("src/day1/input", "r") as f:
         return [
-            ("R" if line[0] == "R" else "L", int(line.strip()[1:]))
+            (-1 if line[0] == "R" else 1) * int(line.strip()[1:])
             for line in f.readlines()
         ]
 
 
-def generate_dial() -> deque:
+def generate_dial() -> deque[int]:
     dial = deque(list(range(0, 100)))
     dial.rotate(50)
     return dial
 
 
 def solve_part_one() -> int:
-    sum = 0
+    def count_zeros(dial: deque[int], turn: int) -> int:
+        dial.rotate(turn)
+        return 1 if dial[0] == 0 else 0
+
     dial = generate_dial()
-    for direction, amount in read_instructions():
-        match direction:
-            case "R":
-                dial.rotate(-amount)
-            case "L":
-                dial.rotate(amount)
-        if dial[0] == 0:
-            sum += 1
-    return sum
+    return sum(count_zeros(dial, turn) for turn in read_instructions())
 
 
 def solve_part_two() -> int:
-    dial = generate_dial()
-    sum = 0
-    for direction, amount in read_instructions():
+    def count_zeros(dial: deque[int], turn: int) -> int:
         last_dial = dial[0]
-        match direction:
-            case "R":
-                dial.rotate(-amount)
-            case "L":
-                dial.rotate(amount)
+        dial.rotate(turn)
         current_dial = dial[0]
-        sum = sum + (amount // 100)
-        if direction == "L" and last_dial < current_dial and last_dial != 0:
-            sum += 1
-        elif direction == "R" and last_dial > current_dial:
-            sum += 1
-        elif last_dial == 0 and current_dial == 0:
-            sum -= 1
-        elif current_dial == 0:
-            sum += 1
-    return sum
+        zeros = abs(turn) // 100
+        if 0 < turn and last_dial < current_dial and last_dial != 0:
+            return zeros + 1
+        if turn < 0 and current_dial < last_dial:
+            return zeros + 1
+        if current_dial == 0:
+            return zeros + 1
+        return zeros
+
+    dial = generate_dial()
+    return sum(count_zeros(dial, turn) for turn in read_instructions())
 
 
 def solve():
