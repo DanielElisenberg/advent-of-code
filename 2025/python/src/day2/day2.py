d672591ad323a3821d947d67633aa79db0f7f810
DanielElisenberg <Daniel@Elisenberg.com>
2025-12-02 06:42:12 +0100

    2025: day 2 - formatting

--- 2025/python/src/day2/day2.py
+++ 2025/python/src/day2/day2.py
@@ -3,7 +3,8 @@ def read_ranges() -> list[range]:
         return [
             range(int(pid_range[0]), int(pid_range[1]) + 1)
             for pid_range in [
-                pid_range.split("-") for pid_range in f.read().strip().split(",")
+                pid_range.split("-")
+                for pid_range in f.read().strip().split(",")
             ]
         ]
 
@@ -17,13 +18,16 @@ def solve_part_1(pid_range: range) -> int:
 
 def solve_part_2(pid_range: range) -> int:
     def is_invalid(id: str) -> bool:
-        for size in range(1, (len(id) // 2)+1):
+        for size in range(1, (len(id) // 2) + 1):
             if len(id) % size != 0:
                 continue
-            id_chunks = [id[start:start+size] for start in range(0, len(id), size)]
+            id_chunks = [
+                id[start : start + size] for start in range(0, len(id), size)
+            ]
             if all(id_chunk == id_chunks[0] for id_chunk in id_chunks):
                 return True
         return False
+
     return sum([int(id) for id in pid_range if is_invalid(str(id))])
 
 
